881dfda68aeb256add923c7da56e6faeb3d9aac6
hariom10555-ops <hariom10555@gmail.com>
2026-04-01 13:51:10 +0530

    first commit

---
+++ home.php
@@ -0,0 +1,109 @@
+<?php
+session_start();
+include 'db.php';
+$result = $conn->query("select * from project");
+?>
+<!doctype html>
+<html lang="en">
+    <head>
+        <title>Title</title>
+        <!-- Required meta tags -->
+        <meta charset="utf-8" />
+        <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+
+        <!-- Bootstrap CSS v5.2.1 -->
+        <link
+            href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+            rel="stylesheet"
+            integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
+            crossorigin="anonymous"
+        />
+    </head>
+
+    <body>
+        <header>
+            <!-- place navbar here -->
+             <nav
+                class="navbar navbar-expand-sm navbar-light bg-light"
+             >
+                <div class="container">
+                    <a class="navbar-brand" href="#"><h3>hello <?php echo $_SESSION["username"]?></h3></a>
+                    <button
+                        class="navbar-toggler d-lg-none"
+                        type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target="#collapsibleNavId"
+                        aria-controls="collapsibleNavId"
+                        aria-expanded="false"
+                        aria-label="Toggle navigation"
+                    >
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="collapsibleNavId">
+                       
+                        <form class="d-flex my-2 my-lg-0">
+                        
+                            <a href="logout.php"
+                                class="btn btn-outline-success my-2 my-sm-0 py-2"
+                                type="submit"
+                            >
+                                Logout
+                            </a>
+
+                        
+                        </form>
+                    </div>
+                </div>
+             </nav>
+             
+        </header>
+        <main>
+             <div
+                class="container"
+            >
+                
+            <div
+                class="table-responsive my-4 py-4"
+            >
+                <table
+                    class="table table-bordered table-hover shadow text-center"
+                >
+                    <thead>
+                        <tr>
+                            <th>USERNAME</th>
+                            <th>PASSWORD</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php while($row = $result->fetch_assoc()){ ?>
+                        <tr>
+                            <td><?php echo $row["username"]; ?></td>
+                            <td><?php echo $row["password"]; ?></td>
+                        </tr>
+                        <?php } ?>
+                    </tbody>
+                </table>
+            </div>
+            
+            </div>
+        </main>
+        <footer>
+            <!-- place footer here -->
+        </footer>
+        <!-- Bootstrap JavaScript Libraries -->
+        <script
+            src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+            integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
+            crossorigin="anonymous"
+        ></script>
+
+        <script
+            src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
+            integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+"
+            crossorigin="anonymous"
+        ></script>
+    </body>
+</html>
